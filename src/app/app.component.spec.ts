84eeb306083f290eef7759caab2eebc77546fdaa
James World <James.World@clarksons.com>
2020-02-28 13:30:37 +0000

    Added tests for VersionService

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { never } from 'rxjs';
+import { VersionService } from './version.service';
+
+class MockVersionService {
+  loadedVersion = "1.0.0";
+  versionChanged = never();
+}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
       ],
+      providers: [{provide: VersionService, useClass: MockVersionService}],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
@@ -26,6 +36,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('SiteUpdaterDemo app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('SiteUpdaterDemo');
   });
 });
--- src/app/version.service.spec.ts
+++ src/app/version.service.spec.ts
@@ -1,16 +1,89 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick, flushMicrotasks } from '@angular/core/testing';
 
-import { VersionService } from './version.service';
+import { VersionService, VersionCheck, UpgradeRecommendation } from './version.service';
+import { of, timer } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map, first, take } from 'rxjs/operators';
+
+class MockHttpClient {
+  get<T>(version: string) {
+    return of("1.0.0");
+  }
+}
 
 describe('VersionService', () => {
   let service: VersionService;
+  let mockHttpClient : MockHttpClient;
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockHttpClient = new MockHttpClient();
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useValue: mockHttpClient }]
+    });
     service = TestBed.inject(VersionService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have initial version', () => {
+    expect(service.loadedVersion).toBe("1.0.0");
+  })
+
+  it('should not report same version', <any>fakeAsync(() => {
+    let flag = false;
+
+    let sub = service.versionChanged.pipe(take(1)).subscribe(result => {
+      flag = true;
+    });
+
+    tick(2000);
+
+    sub.unsubscribe();
+
+    expect(flag).toBeFalse();
+
+  }));
+
+  it('should report minor version change as a recommended upgrade', <any>fakeAsync(() => {
+
+    let versionCheck: VersionCheck;
+
+    spyOn(mockHttpClient, 'get').and.returnValue(of("1.1.0"));
+
+    let sub = service.versionChanged.pipe(take(1)).subscribe(result => {
+      versionCheck = result;
+    });
+
+    tick(2000);
+
+    expect(versionCheck.loaded).toBe("1.0.0");
+    expect(versionCheck.latest).toBe("1.1.0");
+    expect(versionCheck.recommendation).toBe(UpgradeRecommendation.Recommended);
+
+    sub.unsubscribe();
+
+  }));
+
+  it('should report major version change as a recommended upgrade', <any>fakeAsync(() => {
+
+    let versionCheck: VersionCheck;
+
+    spyOn(mockHttpClient, 'get').and.returnValue(of("2.0.0"));
+
+    let sub = service.versionChanged.pipe(take(1)).subscribe(result => {
+      versionCheck = result;
+    });
+
+    tick(2000);
+
+    expect(versionCheck.loaded).toBe("1.0.0");
+    expect(versionCheck.latest).toBe("2.0.0");
+    expect(versionCheck.recommendation).toBe(UpgradeRecommendation.Mandatory);
+
+    sub.unsubscribe();
+
+  }));
 });
