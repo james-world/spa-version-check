082bbcfb5cba4540c17ec4d6509542a53079cbff
James World <James.World@clarksons.com>
2020-02-28 10:04:57 +0000

    Add more meaning types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
-import { VersionService } from './version.service';
+import { Component, OnInit } from '@angular/core';
+import { VersionService, UpgradeRecommendation, VersionCheck } from './version.service';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  version = '';
+  versionNotice = '';
 
   constructor(private versionService: VersionService) {
-    this.versionService.versionChanged.subscribe(
-      {
-        next: _ => {
-          console.log('new version!');
-          window.location.reload();
-        }
-      }
-    );
   }
+  
+  ngOnInit(): void {
+    this.versionNotice = `Current version: ${this.versionService.loadedVersion}`;
+    this.versionService.versionChanged.subscribe({
+      next: v => this.evaluateVersion(v)
+    })
+  }
+
+  private evaluateVersion(v: VersionCheck) {
+    switch(v.recommendation) {
+      case UpgradeRecommendation.Mandatory:
+        this.versionNotice = `Mandatory verison update from ${v.loaded} to ${v.latest}`;
+        timer(5000).subscribe({
+          next: _ => window.location.reload()
+        });
+      case UpgradeRecommendation.Recommended:
+        this.versionNotice = `Recommended verison update from ${v.loaded} to ${v.latest}`;
+    }
+  }
+
 
   title = 'SiteUpdaterDemo';
 }
--- src/app/version.service.ts
+++ src/app/version.service.ts
@@ -1,16 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Version } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, timer, empty } from 'rxjs';
 import { switchMap, map, filter, catchError } from 'rxjs/operators';
+import * as semver from 'semver';
+import { version } from './version';
+
+export enum UpgradeRecommendation {
+  None = 0,
+  Recommended = 1,
+  Mandatory = 2
+}
+
+export class VersionCheck {
+  loaded: string;
+  latest: string;
+  recommendation: UpgradeRecommendation;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class VersionService {
 
-  version = '2.0.0';
+  loadedVersion = version;
 
-  versionChanged: Observable<boolean>;
+  versionChanged: Observable<VersionCheck>;
 
   constructor(private http: HttpClient) {
     console.log('Version module constructed');
@@ -18,9 +32,27 @@ export class VersionService {
     this.versionChanged = timer(0, 1000)
       .pipe(
         switchMap(_ => this.getVersionOrEmpty()),
-        filter(v => v !== this.version),
-        map(_ => true)
+        filter(v => v !== this.loadedVersion),
+        map(v => {
+            return {
+              loaded: this.loadedVersion,
+              latest: v,
+              recommendation: this.compareVersions(v) };
+          })
       );
+  }
+
+  private compareVersions(version: string): UpgradeRecommendation {
+    const diff = semver.diff(this.loadedVersion, version);
+    switch (diff) {
+      case 'major':
+        return UpgradeRecommendation.Mandatory;
+      case 'minor':
+      case 'patch':
+        return UpgradeRecommendation.Recommended;
+      default:
+        return UpgradeRecommendation.None;
+    }
   }
 
   private getVersionOrEmpty(): Observable<string> {
